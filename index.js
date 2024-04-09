6d90664a76d80a787d331c129382ef6ad953d1e2
Syed Ali Kazim Shah <162929580+wal1shah@users.noreply.github.com>
2024-04-09 07:04:37 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const userAnswer = await inquirer.prompt([
+    {
+        name: "userNumber",
+        type: "number",
+        message: "Please guess a number to match it with computer",
+    },
+]);
+if (randomNumber === userAnswer.userNumber) {
+    console.log("Hoorah you guessed it correct!");
+}
+else {
+    console.log("Sorry, You coudn't guessed it correctly");
+}
